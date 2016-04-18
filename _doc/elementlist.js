1c3507aac961cf361c91f3ac877b4300606b2ef6
DJohnny <hermajan@gmail.com>
2016-04-18 14:22:52 +0200

    Deleted documentation.

--- _doc/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","folderContent()"],["f","folderContentWithoutDots()"],["f","http_prihlaseni()"],["c","lib\\Lib"],["c","lib\\Loading"],["c","lib\\Math\\Math"],["c","lib\\Net\\Database"],["c","lib\\Net\\Net"],["c","lib\\Time\\Czech"],["c","lib\\Time\\English"],["c","lib\\Time\\Language"],["c","lib\\Time\\Time"],["c","mysqli"],["f","obnovit()"],["f","poslaniEmailu()"],["f","presmerovat()"],["f","tisk()"],["f","upload()"],["f","upozorneni()"],["f","vpred()"],["f","zpet()"]];
