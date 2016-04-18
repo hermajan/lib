6cc89987536c5ba4cf9d673106d09a4b0877eaad
DJohnny <hermajan@gmail.com>
2016-04-18 15:17:14 +0200

    Added Google Maps rendering.

--- geo/_loader.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once dirname(__DIR__)."/Lib.php"; new \lib\Lib();
-
-$loading=new \lib\Loading();
-$loading->loadJS(LIB_DIR."/geo/geolocation.js");
-$loading->loadPHPfiles(__DIR__);
-?>
--- geo/geolocation.js
+++ geo/geolocation.js
@@ -1,22 +1,19 @@
 /**
  * Handling with Geolocation API from HTML5.
- * 
- * @author DJohnny
- * @version 28.10.2012
  */
 
 /**
- * Locate user
+ * Locate user.
+ * @param method Function which handles obtained position.
  */
-function locate() {
-	if(navigator.geolocation) { navigator.geolocation.watchPosition(writePosition, noLocate); }
+function locate(method) {
+	if(navigator.geolocation) { navigator.geolocation.watchPosition(method, noLocate); }
 	else { noLocate(false); }
 }
 
 /**
  * Handling with geolocation errors.
- * 
- * @param error Type of geolocation error
+ * @param error Type of geolocation error.
  */
 function noLocate(error) {
 	switch(error.code) {
@@ -39,9 +36,26 @@ function noLocate(error) {
 
 /**
  * Writes geographical position of a user.
- * 
- * @param position User position
+ * @param position User position.
  */
 function writePosition(position) {
-	document.write(position.coords.latitude+" "+position.coords.longitude);
+	document.write(position.coords.latitude+" "+position.coords.longitude+" "+position.coords.accuracy+" "+position.coords.altitude+" "+position.coords.altitudeAccuracy+" "+position.coords.heading+" "+position.coords.speed+" "+position.timestamp);
+}
+
+/**
+ * Shows geographical position of a user in HTML element.
+ * @param position User position.
+ * @param id ID of element where position is showed.
+ */
+function showPosition(position, id) {
+	document.getElementById(id).innerHTML = position.coords.latitude+" "+position.coords.longitude+" "+position.coords.accuracy+" "+position.coords.altitude+" "+position.coords.altitudeAccuracy+" "+position.coords.heading+" "+position.coords.speed+" "+position.timestamp;
+}
+
+/**
+ * Returns user position in array.
+ * @param position User position.
+ * @returns Array with user position.
+ */
+function returnPosition(position) {
+	return [position.coords.latitude, position.coords.longitude, position.coords.accuracy, position.coords.altitude, position.coords.altitudeAccuracy, position.coords.heading, position.coords.speed, position.timestamp];
 }
