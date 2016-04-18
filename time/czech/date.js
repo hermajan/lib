7c1a7a09933511f0a2e90052ba890e196e9fbf79
DJohnny <hermajan@gmail.com>
2016-04-18 14:26:40 +0200

    Moved time module to the time repository.

--- time/czech/date.js
+++
@@ -1,5 +0,0 @@
-function denVtydnu() {
-  var datum=new Date();
-  var dny=new Array("neděle","pondělí","úterý","středa","čtvrtek","pátek","sobota");
-  document.write(dny[datum.getDay()]);
-}
--- time/english/date.js
+++
@@ -1,5 +0,0 @@
-function dayOfWeek() {
-  var date=new Date();
-  var days=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-  document.write(days[date.getDay()]);
-}
