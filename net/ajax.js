dbb7a8a1c06cb1c6571ec906bb316a63ee63e679
Jan Hermann <hermajan@gmail.com>
2016-07-24 23:59:12 +0200

    Added AJAX.

---
+++ net/ajax.js
@@ -0,0 +1,21 @@
+/**
+ * Does an AJAX request.
+ * @param {function} callback Callback function.
+ * @param {string} url URL for the AJAX request.
+ * @param {boolean} responseType Type of the response ("xml" or "text").
+ */
+function doAjax(callback, url, responseType) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === 4 && xhr.status === 200) {
+			if(responseType === "xml") {
+				callback(xhr.responseXML);
+			}
+			else {
+				callback(xhr.responseText);
+			}
+		}
+	};
+	xhr.open("GET", url, true);
+	xhr.send();
+}
