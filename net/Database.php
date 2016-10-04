f495afce701380b7df9b74238de94e25b26d5815
Jan Hermann <hermajan@gmail.com>
2016-10-04 19:41:04 +0200

    Fixed bugs in Database.

--- net/Database.php
+++ net/Database.php
@@ -1,5 +1,6 @@
 <?php
 namespace Lib\Net;
+use mysqli;
 
 /**
  * Getting data from MySQL database.
@@ -8,15 +9,14 @@ class Database {
     private $connection;
     
     public function __construct($server,$user,$password,$database) {
-        $this->connection=connect($server,$user,$password,$database);
+        $this->connection = $this->connect($server,$user,$password,$database);
     }
     public function __destruct() {
         $this->connection->close();
     }
     
     /**
-     * Connect into database.
-     * 
+     * Connects into database.
      * @param string $server Address of server, which have desired database
      * @param string $user Username, which can login into database 
      * @param string $password Password for username login
@@ -24,60 +24,52 @@ class Database {
      * @return mysqli Connection to desired database
      */
     public function connect($server,$user,$password,$database) {
-        $connection=new mysqli($server,$user,$password,$database);
+        $this->connection = new mysqli($server,$user,$password,$database);
         
-        if($connection->connect_errno) {
-            return die("Can't connect into database:".$connection->connect_error);
+        if($this->connection->connect_errno) {
+            return die("Can't connect into database:".$this->connection->connect_error);
         }
-
-        $connection->query("SET NAMES UTF8") or die("Can't set charset UTF-8: ".$connection->error);
-
-        return $connection;
+        
+        $this->connection->query("SET NAMES UTF8") or die("Can't set charset UTF-8: ".$this->connection->error);
+        return $this->connection;
     }
     
     /**
-     * Closing a connection.
-     * 
+     * Closes a connection.
      * @param mysqli $connection Connection to close
      */
     public function closing($connection) {
         $connection->close();
     }
-
+    
     /**
      * Returns one value from SELECT query.
-     * 
      * @param string $query SELECT query, which can be run
-     * @return string 
+     * @return string Value.
      */
     public function returning($query) {
-       //$connection=connect();
-       $result=$connection->prepare($query) or die("Bad query: ".$connection->error);
+       $result = $this->connection->prepare($query) or die("Bad query: ".$this->connection->error);
        $result->execute();
        $result->bind_result($note);
        $result->fetch();
-     
+       $result->free_result();
        return $note;
-
-     $result->free_result();
-     //$connection->close();
-   }
-   
-   /**
-    * Runs INSERT, UPDATE or DELETE query.
-    * 
-    * @param string $query INSERT, UPDATE or DELETE query, which can be run
-    * @return int Number of affected rows by query
-    */
+    }
+    
+    /**
+     * Runs INSERT, UPDATE or DELETE query.
+     * @param string $query INSERT, UPDATE or DELETE query, which can be run
+     * @return int Number of affected rows by query
+     */
     public function querySQL($query) {
-      //$connection=connect();
-      $result=$connection->prepare($query) or die("Bad query: ".$connection->error);
+      $result = $this->connection->prepare($query) or die("Bad query: ".$this->connection->error);
       
-      $run=$result->execute();
-      if($run==TRUE) { $rows=$result->affected_rows; }
+      $run = $result->execute();
+      if($run == TRUE) { 
+          $rows = $result->affected_rows;
+      }
       
       $result->free_result();
-      //$connection->close();
       return $rows;
     }
 }
