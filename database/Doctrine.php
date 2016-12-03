74f31a0ff7ac41af2b08af0d708a81420fb94ddc
Jan Hermann <hermajan@gmail.com>
2016-12-03 15:58:40 +0100

    Added Doctrine.

---
+++ database/Doctrine.php
@@ -0,0 +1,54 @@
+<?php
+namespace Lib\Database;
+use Doctrine\DBAL as dbal;
+use Doctrine\DBAL\Connection;
+
+/**
+ * Getting data from MySQL database via Doctrine.
+ */
+class Doctrine {
+	private $connection, $parameters;
+	
+	public function getConnection() {
+		return $this->connection;
+	}
+	
+	public function __construct($configFile) {
+		$ini = parse_ini_file($configFile);
+		
+		$this->parameters = array(
+			"host" => $ini["server"], "dbname" => $ini["database"],
+			"user" => $ini["user"], "password" => $ini["password"],
+			"driver" => "pdo_mysql", "charset"  => "utf8",
+			"driverOptions" => array(1002 => "SET NAMES UTF8")
+		);
+		$this->connect($this->parameters);
+	}
+	public function __destruct() {
+		$this->connection->close();
+	}
+	
+	/**
+	 * Connects into database.
+	 * @param array $parameters Array of parameters.
+	 * @return Connection Connection to desired database.
+	 */
+	public function connect($parameters = null) {
+		if(is_null($parameters)) {
+			$parameters = $this->parameters;
+		}
+		$config = new dbal\Configuration();
+		
+		$this->connection = dbal\DriverManager::getConnection($parameters, $config);
+		return $this->connection;
+	}
+	
+	/**
+	 * Closes a connection.
+	 * @param Connection $connection Connection to close.
+	 */
+	public function closing($connection) {
+		$connection->close();
+	}
+}
+?>
