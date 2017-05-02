39cbe0a7665e6f1cace25846ae0b433fe59b696f
Jan Hermann <hermajan@gmail.com>
2017-05-03 01:23:00 +0200

    Added Nette Forms label.

--- src/nette/Forms.php
+++ src/nette/Forms.php
@@ -16,4 +16,19 @@ class Forms {
 			}
 		}
 	}
+
+	/**
+	 * Returns element for form label with Font Awesome icon.
+	 * @param string $text Text of the label.
+	 * @param string $icon Name of the Font Awesome icon.
+	 * @return \Nette\Utils\Html HTML element for label.
+	 */
+	public static function label($text, $icon=null) {
+		$content = "";
+		if(isset($icon)) {
+			$content .= '<i class="fa fa-'.$icon.'" aria-hidden="true"></i> ';
+		}
+		$content .= $text;
+		return \Nette\Utils\Html::el()->setHtml($content);
+	}
 }
