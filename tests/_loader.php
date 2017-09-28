60c8461f825cb17722ea9bd68dc792e4d44750cf
Jan Hermann <hermajan@gmail.com>
2017-09-28 14:08:28 +0200

    Moved Nette things into Fazette repository.

--- tests/_loader.php
+++ tests/_loader.php
@@ -7,8 +7,5 @@ $lib = new Lib\Lib();
 
 $loading = new Lib\Loading();
 $loading->loadPHPfolders($src, ["phpinfo.php"]);
-$loading->loadPHPfolders($src."/nette/renderers");
 
 //$loading->loadJS(LIB_DIR."/script.js");
-
-\Tracy\Debugger::enable(\Tracy\Debugger::DEVELOPMENT);
--- tests/_testSetup.php
+++ tests/_testSetup.php
@@ -3,5 +3,3 @@ require_once __DIR__."/_loader.php";
 
 Tester\Environment::setup();
 date_default_timezone_set("Europe/Prague");
-
-define("TMP_DIR", "/temp");
