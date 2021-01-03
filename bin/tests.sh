473e32df087c2daf50e3b969ce05baa846e54991
Jan Hermann <hermajan@gmail.com>
2021-01-03 01:51:10 +0100

    Updated tests

---
+++ bin/tests.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+# Script runs tests
+
+COMMAND=$1
+
+function help() {
+    echo "all - runs all tests"
+    echo "ci - runs tests for continuous integration"
+    echo "cypress - runs Cypress"
+    echo "phpstan - runs PHPStan"
+    echo "tester - runs Nette Tester tests"
+}
+
+# Runs Cypress
+function cypress() {
+    echo -e "Cypress"
+    ./node_modules/cypress/bin/cypress open
+}
+
+# Runs PHPStan
+function phpstan() {
+    echo -e "PHPStan"
+    ./vendor/bin/phpstan analyse app -c app/config/phpstan.neon
+}
+
+# Runs Nette Tester tests
+function tester() {
+    # Default shell script for running tests from `tests` folder
+    echo -e "Nette Tester"
+
+    # PHP version of script for running tests
+    php "./vendor/nette/tester/src/tester.php" -C tests
+}
+
+function all() {
+#    phpstan
+    tester
+}
+
+function ci() {
+	local results=0
+    phpstan || results=$((results+$?))
+    tester || results=$((results+$?))
+    return ${results}
+}
+
+if [[ "${COMMAND}" == "" ]]; then
+    help
+fi
+
+# run command
+$COMMAND
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,11 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 
-$src = __DIR__."/../src";
-require_once $src."/Lib.php"; 
-$lib = new Lib\Lib();
-
-$loading = new Lib\Loading();
-$loading->loadPHPfolders($src, ["phpinfo.php"]);
-
 $temp = __DIR__."/../.temp";
 if(!is_dir($temp)) {
 	mkdir($temp, 0777, true);
 }
 
 Tester\Environment::setup();
-Tester\Dumper::$dumpDir = $temp."/output";
+Tester\Dumper::$dumpDir = $temp."/tester";
 date_default_timezone_set("Europe/Prague");
